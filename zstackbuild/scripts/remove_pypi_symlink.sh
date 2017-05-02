7be977eed24d0554c8c928e83d5626eb73c6da45
Harvis Wang <jiankangshiye@aliyun.com>
2017-05-02 16:49:49 +0800

    support compile on FreeBSD

--- zstackbuild/scripts/remove_pypi_symlink.sh
+++ zstackbuild/scripts/remove_pypi_symlink.sh
@@ -3,8 +3,6 @@ for item in `find $1`; do
     if [ -L $item ]; then
         echo "Remove symbolic for $item"
         cd `dirname $item`
-        base_file=`readlink $item`
-        cp --remove-destination `readlink $item` $item
-        rm -f $base_file
+        mv `readlink $item` $item
     fi
 done
